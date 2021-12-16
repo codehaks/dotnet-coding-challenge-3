2fa4817dc8faa844d742300b1fd89c776570f531
Mina Tahmasebi <mina_t_b@outlook.com>
2021-12-16 13:30:25 +0100

    Add project files.

---
+++ src/sage.challenge.api/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sage.challenge.data.Cache;
+using sage.challenge.data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace sage.challenge.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class UserController : Controller
+    {
+        private readonly ISimpleObjectCache<Guid, User> _cache;
+
+        public UserController(ISimpleObjectCache<Guid, User> cache)
+        {
+            _cache = cache;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(User user)
+        {
+            await _cache.AddAsync(user.Id, user);
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _cache.GetAllAsync();
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetUser(Guid id)
+        {
+            var user = await _cache.GetAsync(id);
+            return Ok(user);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            await _cache.DeleteAsync(id);
+            return Ok(id);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutUser(User user)
+        {
+            await _cache.UpdateAsync(user.Id, user);
+            return Ok(user);
+        }
+    }
+}
